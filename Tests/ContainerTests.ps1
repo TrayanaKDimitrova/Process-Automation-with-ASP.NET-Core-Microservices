7f888bf35d1ab96789b52d1ee6f00efe7160d77f
TrayanaKDimitrova <trayana.k.dimitrova@gmail.com>
2020-11-26 12:55:05 +0200

    Test admin panel and watchdog started.

--- Tests/ContainerTests.ps1
+++ Tests/ContainerTests.ps1
@@ -4,8 +4,21 @@ do {
     Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
     
     $testStart = Invoke-WebRequest -Uri http://localhost -UseBasicParsing
+	$adminPanelStart = Invoke-WebRequest -Uri http://localhost:5000/ -UseBasicParsing
+	$watchdogStart = Invoke-WebRequest -Uri http://localhost:5500/healthchecks -UseBasicParsing
     
     if ($testStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 1
+    }
+	
+	if ($adminPanelStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 1
+    }
+	if ($watchdogStart.statuscode -eq '200') {
         $started = $true
     } else {
         Start-Sleep -Seconds 1
