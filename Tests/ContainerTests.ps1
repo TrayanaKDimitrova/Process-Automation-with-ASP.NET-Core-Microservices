682ffe70fa1302197888deed12cc949f00e6e77f
TrayanaKDimitrova <trayana.k.dimitrova@gmail.com>
2020-11-26 18:08:14 +0200

    Add test loading pages

--- Tests/ContainerTests.ps1
+++ Tests/ContainerTests.ps1
@@ -3,10 +3,19 @@ do {
     $count++
     Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
     
+	Write-Output "Start test load pages"
     $testStart = Invoke-WebRequest -Uri http://localhost -UseBasicParsing
 	$adminPanelStart = Invoke-WebRequest -Uri http://localhost:5000/ -UseBasicParsing
 	$watchdogStart = Invoke-WebRequest -Uri http://localhost:5500/healthchecks -UseBasicParsing
+    $statisticsStart = Invoke-WebRequest -Uri http://localhost:5003/statistics -UseBasicParsing
+    $carPageStart = Invoke-WebRequest -Uri http://localhost:5002/carads/ -UseBasicParsing
     
+	Write-Output "Test nomeclature"
+	$nomeclatureLoad = Invoke-WebRequest -Uri http://localhost:5002/carads/categories -UseBasicParsing
+	
+	Write-Output "Test load create page"
+	$createPage = Invoke-WebRequest -Uri http://localhost/cars/create - -UseBasicParsing
+
     if ($testStart.statuscode -eq '200') {
         $started = $true
     } else {
@@ -23,7 +32,28 @@ do {
     } else {
         Start-Sleep -Seconds 1
     }
-    
+	
+	if ($statisticsStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 1
+    }
+	
+	if ($carPageStart.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 1
+    }
+	if ($nomeclatureLoad.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 1
+    }
+	if ($createPage.statuscode -eq '200') {
+        $started = $true
+    } else {
+        Start-Sleep -Seconds 1
+    }
 } until ($started -or ($count -eq 3))
 
 if (!$started) {
